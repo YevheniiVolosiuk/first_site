2a4ca8c390163acb4992bc7c6c5dc7fb7d4fc75b
YEVHENII VOLOSIUK <volosiukweb@gmail.com>
2021-01-17 21:00:10 +0100

    Revert "nav menuLinks remove"
    This reverts commit 8dddc882ea0796d4153aa2f6e0e88c62870045d4.

--- js/main-es6.js
+++ js/main-es6.js
@@ -17,20 +17,12 @@
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header__nav');
     const menuCloseItem = document.querySelector('.header__nav-close');
-    const menuLinks = document.querySelectorAll('.header__link');
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header__nav-active');
     });
     menuCloseItem.addEventListener('click', () => {
         menu.classList.remove('header__nav-active');
     });
-    if (window.innerWidth <= 767) {
-        for  (let i = 0; i < menuLinks.length; i += 1) {
-            menuLinks[1].addEventListener('click', () => {
-                menu.classList.remove('header__nav-active');
-            });
-        }
-    }
 }());
 
 // Scroll to anchors
